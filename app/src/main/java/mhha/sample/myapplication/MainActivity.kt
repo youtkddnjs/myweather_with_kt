a3aa2b690b2defb146a000fac3bd9999dffef744
youtkddnjs <youtkddnjs91@gmail.com>
2023-11-22 09:50:00 +0900

    Add 위치 정보를 통한 API 호출

--- app/src/main/java/mhha/sample/myapplication/MainActivity.kt
+++ app/src/main/java/mhha/sample/myapplication/MainActivity.kt
@@ -1,10 +1,13 @@
 package mhha.sample.myapplication
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
@@ -20,96 +23,34 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.O)
     val locationPermissionRequest = registerForActivityResult(
         ActivityResultContracts.RequestMultiplePermissions()
     ) { permissions ->
         when {
             permissions.getOrDefault(android.Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
-                // Precise location access granted.
+                updateLocation()
             }
             permissions.getOrDefault(android.Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
-                // Only approximate location access granted.
+                updateLocation()
             } else -> {
                 Toast.makeText(this,"워치 권한 필요함",Toast.LENGTH_SHORT).show()
-        }
+                val intent = Intent(Settings.ACTION_APPLICATION_SETTINGS).apply{
+                    data = Uri.fromParts("lackage",packageName,null)
+                }
+                startActivity(intent)
+                finish()
+            }
         }
     }
 
-// ...
-
-// Before you perform the actual permission request, check whether your app
-// already has the permissions, and whether your app needs to show a permission
-// rationale dialog. For more details, see Request permissions.
-
-
     private lateinit var binding : ActivityMainBinding
-    @RequiresApi(Build.VERSION_CODES.O)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //위치권한
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED )
-        {
-            locationPermissionRequest.launch(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION))
-            return
-        }
-        val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        fusedLocationClient?.lastLocation?.addOnSuccessListener {
-            Log.d("lastLocation", it?.toString() ?: "none" )
-        }
-
-        val retrofit= Retrofit.Builder()
-            .baseUrl("http://apis.data.go.kr/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        val service = retrofit.create(WeatherService::class.java)
-
-        val baseDateTime = BaseDateTime.getBaseDateTime()
-
-        val converter = GeoPointConverter()
-        val point = converter.convert(lon = 127.04 , lat = 37.51)
-        service.getVillageForescast(
-            servicekey = "djc2Y0AjqXY1scaDhW/GnRjusKgsTFTW70ThCP/x8E2f9XeA1dUDVhP6RypcGM67pNPxPvrFQuGpYI4hQkGVOw==",
-            baseDate = baseDateTime.baseDate,
-            baseTime = baseDateTime.baseTime,
-            nx = point.nx,
-            ny = point.ny
-        ).enqueue(object : Callback<WeatherEntity>{
-            override fun onResponse(call: Call<WeatherEntity>, response: Response<WeatherEntity>) {
-                val forecastDataTimeMap = mutableMapOf<String, Forecast>()
-                val forecastList = response.body()?.response?.body?.items?.forecastEntities.orEmpty()
-                for (i in forecastList){
-
-//                    Log.i("Forecast", i.toString())
-
-                    if(forecastDataTimeMap["${i.forecastDate}/${i.forecastTime}"] == null){
-                        forecastDataTimeMap["${i.forecastDate}/${i.forecastTime}"] =
-                            Forecast(forecastDate = i.forecastDate, forecastTime = i.forecastTime)
-                    }
-
-                    forecastDataTimeMap["${i.forecastDate}/${i.forecastTime}"]?.apply {
-                        when(i.category){
-                            Category.POP.toString() -> {precipitation = i.forecastValue.toInt()}
-                            Category.PTY.toString() -> {precipitationType = transformRainType(i)}
-                            Category.SKY.toString() -> {sky = transformSkyType(i)}
-                            Category.TMP.toString() -> {temperature = i.forecastValue.toDouble()}
-                            else -> {}
-                        }
-                    }//forecastDataTimeMap["${i.forecastDate}/${i.forecastTime}"].apply
-                }//for (i in forecastList)
-                Log.i("Forecast", forecastDataTimeMap.toString())
-                Log.i("Forecast", "${baseDateTime.baseDate}, ${baseDateTime.baseTime }")
-            }//override fun onResponse(call: Call<WeatherEntity>, response: Response<WeatherEntity>)
-
-            override fun onFailure(call: Call<WeatherEntity>, t: Throwable) {
-                t.printStackTrace()
-            }
-        })
 
     }//override fun onCreate(savedInstanceState: Bundle?)
 
@@ -133,5 +74,67 @@ class MainActivity : AppCompatActivity() {
             else -> ""
         }
     } //private fun transformSkyType(forecast: ForecastEntity): String
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun updateLocation(){
+        //위치권한
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED )
+        {
+            locationPermissionRequest.launch(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION))
+            return
+        }
+        val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationClient?.lastLocation?.addOnSuccessListener {
+            val retrofit= Retrofit.Builder()
+                .baseUrl("http://apis.data.go.kr/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+            val service = retrofit.create(WeatherService::class.java)
+
+            val baseDateTime = BaseDateTime.getBaseDateTime()
+
+            val converter = GeoPointConverter()
+            val point = converter.convert(lon = it.longitude , lat = it.latitude)
+            service.getVillageForescast(
+                servicekey = "djc2Y0AjqXY1scaDhW/GnRjusKgsTFTW70ThCP/x8E2f9XeA1dUDVhP6RypcGM67pNPxPvrFQuGpYI4hQkGVOw==",
+                baseDate = baseDateTime.baseDate,
+                baseTime = baseDateTime.baseTime,
+                nx = point.nx,
+                ny = point.ny
+            ).enqueue(object : Callback<WeatherEntity>{
+                override fun onResponse(call: Call<WeatherEntity>, response: Response<WeatherEntity>) {
+                    val forecastDataTimeMap = mutableMapOf<String, Forecast>()
+                    val forecastList = response.body()?.response?.body?.items?.forecastEntities.orEmpty()
+                    for (i in forecastList){
+
+//                    Log.i("Forecast", i.toString())
+
+                        if(forecastDataTimeMap["${i.forecastDate}/${i.forecastTime}"] == null){
+                            forecastDataTimeMap["${i.forecastDate}/${i.forecastTime}"] =
+                                Forecast(forecastDate = i.forecastDate, forecastTime = i.forecastTime)
+                        }
+
+                        forecastDataTimeMap["${i.forecastDate}/${i.forecastTime}"]?.apply {
+                            when(i.category){
+                                Category.POP.toString() -> {precipitation = i.forecastValue.toInt()}
+                                Category.PTY.toString() -> {precipitationType = transformRainType(i)}
+                                Category.SKY.toString() -> {sky = transformSkyType(i)}
+                                Category.TMP.toString() -> {temperature = i.forecastValue.toDouble()}
+                                else -> {}
+                            }
+                        }//forecastDataTimeMap["${i.forecastDate}/${i.forecastTime}"].apply
+                    }//for (i in forecastList)
+                    Log.i("Forecast", forecastDataTimeMap.toString())
+                    Log.i("Forecast", "${baseDateTime.baseDate}, ${baseDateTime.baseTime }")
+                }//override fun onResponse(call: Call<WeatherEntity>, response: Response<WeatherEntity>)
+
+                override fun onFailure(call: Call<WeatherEntity>, t: Throwable) {
+                    t.printStackTrace()
+                }
+            })
+        }//fusedLocationClient?.lastLocation?.addOnSuccessListener
+    }//private fun updateLocation()
 
 }//class MainActivity : AppCompatActivity()
